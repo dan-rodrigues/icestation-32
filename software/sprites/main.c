42223eb54fbd1a5e9a8cae54bec7b7d30f70bb8a
Dan Rodrigues <danrr.gh.oss@gmail.com>
2020-05-22 18:53:56 +1000

    Move a few config options to the top

--- software/sprites/main.c
+++ software/sprites/main.c
@@ -9,6 +9,19 @@
 
 void draw_crystal_sprite(uint8_t *base_sprite_id, uint16_t base_tile, uint8_t palette, uint16_t x, uint16_t y);
 
+// sprite pattern arrangement
+
+static const uint8_t YELLOW_PALETTE = 0;
+static const uint8_t RED_PALETTE = 1;
+static const uint8_t GREEN_PALETTE = 2;
+static const uint8_t MAGENTA_PALETTE = 3;
+
+// background colors to cycle through
+// note the format is ARGB16 so all example colors for the background are alpha = 100%
+static const uint16_t CYCLED_BACKGROUND_COLORS[] = {0xf088, 0xf600, 0xf888, 0xf000};
+
+// sprite circle arrangements
+
 typedef struct {
     uint8_t sprite_count;
     uint16_t angle_delta;
@@ -16,21 +29,39 @@ typedef struct {
     uint8_t palette;
 } CircleArrangement;
 
-/*
- static const int16_t circle_counts_table[] = {32, 16, 8, 1};
- static const int16_t circle_angle_delta_table[] = {SIN_MAX / 32, SIN_MAX / 16, SIN_MAX / 8, SIN_MAX / 1};
- static const int16_t circle_radius_table[] = {192, 128, 64, 0};
- static const uint8_t circle_palette_ids[] = {yellow_palette, green_palette, red_palette, magenta_palette};
- */
+static const CircleArrangement CIRCLE_ARRANGEMENTS[] = {
+    {.sprite_count = 32, .angle_delta = SIN_PERIOD / 32, .radius = 192, .palette = YELLOW_PALETTE},
+    {.sprite_count = 16, .angle_delta = SIN_PERIOD / 16, .radius = 128, .palette = GREEN_PALETTE},
+    {.sprite_count = 8, .angle_delta = SIN_PERIOD / 8, .radius = 64, .palette = RED_PALETTE},
+    {.sprite_count = 1, .angle_delta = SIN_PERIOD / 1, .radius = 0, .palette = MAGENTA_PALETTE},
+};
+
+static const size_t CIRCLE_ARRANGEMENT_COUNT = sizeof(CIRCLE_ARRANGEMENTS) / sizeof(CircleArrangement);
+
+// sprite animation metadata
+
+static const uint8_t CRYSTAL_ANIMATION_FRAMES = 8;
+static const uint8_t CRYSTAL_FRAME_WIDTH = 32;
+
+// options to toggle
+
+// layers must be opted into alpha compositing
+// without it enabled, the "alpha" channel is treated as an intensity from black to the original color
+// with it enabled, the alpha channel is used to blend with the topmost opaque layer beneath it
+static const bool ENABLE_SPRITE_ALPHA = true;
+
+// this enables immediate animation for use in the simulator since the frame rate is much lower
+static const bool ENABLE_FAST_ANIMATION = false;
 
 int main() {
+    // layer configuration (only sprites are used here)
     vdp_enable_layers(SPRITES);
-    vdp_set_alpha_over_layers(SPRITES);
+    vdp_set_alpha_over_layers(ENABLE_SPRITE_ALPHA ? SPRITES : 0);
 
     const uint16_t tile_base = 0x0000;
     vdp_set_sprite_tile_base(tile_base);
 
-    // tiles
+    // tile upload
 
     vdp_set_vram_increment(1);
     vdp_seek_vram(tile_base);
@@ -40,12 +71,7 @@ int main() {
         vdp_write_vram(tiles[i] >> 16);
     }
 
-    // palette
-
-    const uint8_t yellow_palette = 0;
-    const uint8_t red_palette = 1;
-    const uint8_t green_palette = 2;
-    const uint8_t magenta_palette = 3;
+    // palette upload
 
     vdp_seek_palette(0);
 
@@ -67,54 +93,28 @@ int main() {
         vdp_write_palette_color((color & 0xff00) | (color & 0x00f0) >> 4);
     }
 
-    // background color
-
-    // teal
-    vdp_set_single_palette_color(0, 0xf088);
-
-    vdp_set_single_palette_color(0, 0xf800);
-
-    // sprite pattern arrangement
-    
-    const int16_t screen_center_x = 848 / 2;
-    const int16_t screen_center_y = 480 / 2;
-
-    // first row of 32x32 animation frames
-    const uint8_t crystal_frames_bank_0 = 0x000;
-    // second row of 32x32 animation frames
-    const uint8_t crystal_frames_bank_1 = 0x040;
-
-    const uint8_t crystal_total_frames = 8;
-    const uint8_t crystal_frame_width = 32;
-
     uint16_t frame_counter = 0;
-
     uint8_t base_sprite_id = 0;
 
     vdp_clear_all_sprites();
 
     while(true) {
+        // first row of 32x32 animation frames
+        const uint8_t crystal_frames_bank_0 = 0x000;
+        // second row of 32x32 animation frames
+        const uint8_t crystal_frames_bank_1 = 0x040;
+
         uint8_t crystal_frame = frame_counter / 4;
-        crystal_frame &= (crystal_total_frames - 1);
-        uint16_t crystal_tile_base = (crystal_frame & 3) * crystal_frame_width / 8;
+        crystal_frame &= (CRYSTAL_ANIMATION_FRAMES - 1);
+        uint16_t crystal_tile_base = (crystal_frame & 3) * CRYSTAL_FRAME_WIDTH / 8;
         crystal_tile_base += (crystal_frame & 4) ? crystal_frames_bank_1 : crystal_frames_bank_0;
 
-        static const CircleArrangement circle_arrangements[] = {
-            {.sprite_count = 32, .angle_delta = SIN_PERIOD / 32, .radius = 192, .palette = yellow_palette},
-            {.sprite_count = 16, .angle_delta = SIN_PERIOD / 16, .radius = 128, .palette = green_palette},
-            {.sprite_count = 8, .angle_delta = SIN_PERIOD / 8, .radius = 64, .palette = red_palette},
-            {.sprite_count = 1, .angle_delta = SIN_PERIOD / 1, .radius = 0, .palette = magenta_palette},
-        };
+        uint8_t back_color_index = frame_counter / (ENABLE_FAST_ANIMATION ? 1: 0x80) & 3;
 
-        const size_t circle_count = sizeof(circle_arrangements) / sizeof(CircleArrangement);
+        vdp_set_single_palette_color(0, CYCLED_BACKGROUND_COLORS[back_color_index]);
 
-        static const uint16_t back_color_table[] = {0xf088, 0xf800, 0xf888, 0xf000};
-//        uint8_t back_color_index = frame_counter / 0x80 & 3;
-        uint8_t back_color_index = frame_counter & 3;
-        vdp_set_single_palette_color(0, back_color_table[back_color_index]);
-
-        for (uint8_t circle = 0; circle < circle_count; circle++) {
-            CircleArrangement arrangement = circle_arrangements[circle];
+        for (uint8_t circle = 0; circle < CIRCLE_ARRANGEMENT_COUNT; circle++) {
+            CircleArrangement arrangement = CIRCLE_ARRANGEMENTS[circle];
 
             int16_t radius = arrangement.radius;
             uint8_t palette = arrangement.palette;
@@ -124,11 +124,14 @@ int main() {
             uint16_t angle = frame_counter;
 
             for (uint8_t i = 0; i < sprite_count; i++) {
+                const int16_t screen_center_x = 848 / 2;
+                const int16_t screen_center_y = 480 / 2;
+
                 int16_t sin_t = sin(angle);
                 int16_t cos_t = cos(angle);
 
-                int16_t sprite_x = sys_multiply(cos_t, -radius) / 0x4000;
-                int16_t sprite_y = sys_multiply(sin_t, -radius) / 0x4000;
+                int16_t sprite_x = sys_multiply(cos_t, -radius) / SIN_MAX;
+                int16_t sprite_y = sys_multiply(sin_t, -radius) / SIN_MAX;
 
                 sprite_x += screen_center_x;
                 sprite_y += screen_center_y;
