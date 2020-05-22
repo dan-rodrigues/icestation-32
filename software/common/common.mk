883d0e70bd099f97ce774b2459eeb35a873ac623
Dan Rodrigues <danrr.gh.oss@gmail.com>
2020-05-22 20:43:50 +1000

    Relocate to common rules Makefile

---
+++ software/common/common.mk
@@ -0,0 +1,21 @@
+CROSS = riscv-none-embed-
+CFLAGS = -Wall -Os -flto -march=rv32i -I$(dir $(lastword $(MAKEFILE_LIST))) -I$(dir $(lastword $(MAKEFILE_LIST)))../lib/ -I./ -ffreestanding -nostdlib
+DFLAGS = --line-numbers
+ 
+LDS = ../common/sections.lds
+LDS_P = sections_p.lds
+
+prog.bin: prog.elf
+	$(CROSS)objcopy -O binary prog.elf prog.bin
+
+dasm: prog.elf
+	$(CROSS)objdump -d $(DFLAGS) prog.elf > dasm
+
+$(LDS_P): $(LDS)
+	$(CROSS)cpp -P -o $@ $^
+
+prog.elf: $(LDS_P) $(HEADERS) $(SOURCES)
+	$(CROSS)gcc $(CFLAGS) -Wl,-Bstatic,-T,$(LDS_P),--strip-debug -o prog.elf $(SOURCES)
+
+clean:
+	rm -f prog.elf prog.hex prog.bin
