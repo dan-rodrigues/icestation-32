953b8c3c6e742956151781062244439d6862dda9
Dan Rodrigues <danrr.gh.oss@gmail.com>
2020-05-22 21:27:00 +1000

    Add round up for palette conversion

--- utilities/png_convert/intermediate/Palette.cpp
+++ utilities/png_convert/intermediate/Palette.cpp
@@ -3,13 +3,16 @@
 std::vector<uint16_t> Palette::ics_palette() {
     std::vector<uint16_t> ics_palette;
 
-    // TODO: rounding up of colors if needed + clipping
+    auto round = [] (uint8_t component) {
+        return std::min(0x0f, (component + 8) / 0x10);
+    };
+
     for (auto it = begin(rgba32_palette); it != end(rgba32_palette); ++it) {
         uint32_t color = *it;
-        uint8_t a = (color >> 24 & 0xff) >> 4;
-        uint8_t b = (color >> 16 & 0xff) >> 4;
-        uint8_t g = (color >> 8 & 0xff) >> 4;
-        uint8_t r = (color & 0xff) >> 4;
+        uint8_t a = round(color >> 24 & 0xff);
+        uint8_t b = round(color >> 16 & 0xff);
+        uint8_t g = round(color >> 8 & 0xff);
+        uint8_t r = round(color & 0xff);
 
         uint16_t ics_color = a << 12 | r << 8 | g << 4 | b;
         ics_palette.push_back(ics_color);
