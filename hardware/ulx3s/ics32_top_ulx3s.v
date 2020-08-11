77e2932176124b9883a174b54b70b1aff5da7aba
Dan Rodrigues <danrr.gh.oss@gmail.com>
2020-08-11 22:47:40 +1000

    Manually place flash IO with constrained TRELLIS_SLICE This should mitigate most of the PNR randomness since ODDRs aren't used

--- hardware/ulx3s/ics32_top_ulx3s.v
+++ hardware/ulx3s/ics32_top_ulx3s.v
@@ -62,18 +62,47 @@ module ics32_top_ulx3s #(
     wire [3:0] flash_in_en;
     wire [3:0] flash_in;
 
-    // IO
+    // Flash bidirectional IO
 
-    reg [3:0] flash_out_r;
-    reg [3:0] flash_in_r, flash_in_en_r;
+    wire [3:0] flash_out_r;
+    wire [3:0] flash_in_en_r;
+    wire [3:0] flash_in_r;
 
-    always @(posedge clk_2x) begin
-        flash_out_r <= flash_out;
-        flash_in_r <= flash_in;
+    (* BEL="X11/Y93/SLICEA" *) TRELLIS_SLICE flash_in_r_0 (
+        .CLK(clk_2x),
+        .M0(flash_in[0]), .M1(flash_in[1]),
+        .Q0(flash_in_r[0]), .Q1(flash_in_r[1])
+    );
 
-        // These enable inputs must be inverted to be used as an active-high tristate instead
-        flash_in_en_r <= ~flash_in_en;
-    end
+    (* BEL="X11/Y93/SLICEB" *) TRELLIS_SLICE flash_in_r_1 (
+        .CLK(clk_2x),
+        .M0(flash_in[2]), .M1(flash_in[3]),
+        .Q0(flash_in_r[2]), .Q1(flash_in_r[3])
+    );
+
+    (* BEL="X11/Y93/SLICEC" *) TRELLIS_SLICE flash_in_en_r_0 (
+        .CLK(clk_2x),
+        .M0(~flash_in_en[0]), .M1(~flash_in_en[1]),
+        .Q0(flash_in_en_r[0]), .Q1(flash_in_en_r[1])
+    );
+
+    (* BEL="X11/Y93/SLICED" *) TRELLIS_SLICE flash_in_en_r_1 (
+        .CLK(clk_2x),
+        .M0(~flash_in_en[2]), .M1(~flash_in_en[3]),
+        .Q0(flash_in_en_r[2]), .Q1(flash_in_en_r[3])
+    );
+
+    (* BEL="X9/Y93/SLICEA" *) TRELLIS_SLICE flash_out_r_0 (
+        .CLK(clk_2x),
+        .M0(flash_out[0]), .M1(flash_out[1]),
+        .Q0(flash_out_r[0]), .Q1(flash_out_r[1]),
+    );
+
+    (* BEL="X9/Y93/SLICEB" *) TRELLIS_SLICE flash_out_r_1 (
+        .CLK(clk_2x),
+        .M0(flash_out[2]), .M1(flash_out[3]),
+        .Q0(flash_out_r[2]), .Q1(flash_out_r[3]),
+    );
 
     BB flash_io_bb[3:0] (
         .I(flash_in_r),
@@ -82,43 +111,49 @@ module ics32_top_ulx3s #(
         .B(flash_io)
     );
 
-    // CSN
+    // Flash CSN
 
-    reg flash_csn_r;
+    wire flash_csn_r;
 
-    always @(posedge clk_2x) begin
-        flash_csn_r <= flash_csn_io;
-    end
+    (* BEL="X15/Y93/SLICEA" *) TRELLIS_SLICE flash_csn_r_0 (
+        .CLK(clk_2x),
+        .M0(flash_csn_io),
+        .Q0(flash_csn_r)
+    );
 
     OB flash_csn_ob(
         .I(flash_csn_r),
         .O(flash_csn)
     );
 
-    // CLK
+    // SPI CLK
 
     // * The ECP5 SPI clock must be controlled with USRMCLK
     // * DDR primitive can't be used with USRMCLK
+    // * There is no built in supported to route a global clock USRMCLK
 
-    // The below block is meant to work around this constraint but isn't ideal
-    // It is only "safe" because flask_clk_ddr_r[0] is either 0 or a duplicate of flash_clk_ddr[1]
-    // This means the flash clk only rises when clk_2x falls so shouldn't be any setup/hold violation
-
-    reg [1:0] flash_clk_ddr_r;
     wire flash_clk_out = clk_2x ? flash_clk_ddr_r[0] : flash_clk_ddr_r[1];
 
-    always @(posedge clk_2x) begin
-        flash_clk_ddr_r[0] <= flash_clk_ddr[0];
-    end
+    wire [1:0] flash_clk_ddr_r;
 
-    always @(negedge clk_2x) begin
-        flash_clk_ddr_r[1] <= flash_clk_ddr[1];
-    end
+    (* BEL="X2/Y93/SLICEA" *) TRELLIS_SLICE ddr_ff_0 (
+        .CLK(clk_2x),
+        .M0(flash_clk_ddr[0]),
+        .Q0(flash_clk_ddr_r[0])
+    );
 
+    (* BEL="X2/Y92/SLICEA" *) TRELLIS_SLICE #(.CLKMUX("INV")) ddr_ff_1 (
+        .CLK(clk_2x),
+        .M0(flash_clk_ddr[1]),
+        .Q0(flash_clk_ddr_r[1])
+    );
+    
     USRMCLK spi_clk (
         .USRMCLKI(flash_clk_out),
         .USRMCLKTS(1'b0)
     );
+
+    // --- icestation-32 ---
 
     wire flash_csn_io;
     wire [1:0] flash_clk_ddr;
@@ -155,7 +190,7 @@ module ics32_top_ulx3s #(
         .flash_csn(flash_csn_io),
         .flash_in_en(flash_in_en),
         .flash_in(flash_in),
-        .flash_out(flash_out_r),
+        .flash_out(flash_out_r)
     );
 
 endmodule
