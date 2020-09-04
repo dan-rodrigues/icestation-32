8b28836dd68c3307e485728a0a0f202f7f7bc924
Dan Rodrigues <danrr.gh.oss@gmail.com>
2020-09-04 16:26:13 +1000

    Minor optimization of VDP ctrl interface

--- hardware/vdp/vdp_host_interface.v
+++ hardware/vdp/vdp_host_interface.v
@@ -63,49 +63,47 @@ module vdp_host_interface #(
 
     always @(posedge clk) begin
         if (reset) begin
-            data_t <= 0;
             register_write_en <= 0;
-            register_write_address <= 0;
-            register_write_data <= 0;
+            data_t <= 0;
         end else if (USE_8BIT_BUS) begin
-            if (host_write_en_r) begin
-                if (host_address_r[0]) begin
-                    register_write_data <= {host_write_data_r[7:0], data_t};
-                    register_write_address <= host_address_r[5:1];
-                    register_write_en <= 1;
+            if (host_address_r[0]) begin
+                register_write_en <= host_write_en_r;
 
-                    data_t <= 0;
-                end else begin
-                    data_t <= host_write_data_r[7:0];
-                    register_write_en <= 0;
-                end
+                data_t <= 0;
             end else begin
-                register_write_address <= 0; 
+                data_t <= host_write_data_r[7:0];
                 register_write_en <= 0;
             end
         end else begin
-            if (cop_write_en && host_write_en) begin
-                // this is either a software or hardware bug so flag it accordingly
-                `stop($display("CPU / VDP copper write conflict");)
-            end
+            register_write_en <= cop_write_en || (host_write_en && !host_write_en_r);
+        end
+    end
 
-            // if there was a conflict, prioritize the CPU
+    always @(posedge clk) begin
+        if (USE_8BIT_BUS) begin
+            if (host_write_en_r && host_address_r[0]) begin
+                register_write_data <= {host_write_data_r[7:0], data_t};
+                register_write_address <= host_address_r[5:1];
+            end
+        end else begin
+            // If there's a conflict, prioritize CPU
             if (host_write_en) begin
                 register_write_address <= host_address;
                 register_write_data <= host_write_data;
-            end else if (cop_write_en) begin
+            end else begin
                 register_write_address <= cop_write_address;
                 register_write_data <= cop_write_data;
             end
 
             // CPU is always free to read
             read_address <= host_address_r;
+        end
+    end
 
-            if (cop_write_en || (host_write_en && !host_write_en_r)) begin
-                register_write_en <= 1;
-            end else begin
-                register_write_en <= 0;
-            end
+    always @(posedge clk) begin
+        if (!reset && cop_write_en && host_write_en) begin
+            // this is either a software or hardware bug so flag it accordingly
+            `stop($display("CPU / VDP copper write conflict");)
         end
     end
 
